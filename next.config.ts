a56961aa5e1322bf44be7df93a136735d967d318
Ansh Ramanath <anshramanath04@gmail.com>
2026-08-07 14:32:38 -0500

    up the image limit

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
